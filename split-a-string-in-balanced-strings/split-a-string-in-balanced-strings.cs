e871b08a6c2247bccba44b6bf732d84e09d85c1b
MohamedAhmedAbdAlaziz <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2021-12-25 19:49:20 +0200

    Update split-a-string-in-balanced-strings.cs

--- split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cs
+++ split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cs
@@ -16,7 +16,7 @@ public class Solution {
 
                         coR++;
                         ss++;
-                    }//"RLRRRLLRLL"
+                    } 
                     else
                     {
                         coL++;
