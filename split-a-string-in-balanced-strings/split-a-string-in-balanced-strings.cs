1c76cb5291c41980f66f0d55fb044edcaf9f0707
MohamedAhmedAbdAlaziz <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2021-12-25 19:47:51 +0200

    Time: 72 ms (55.15%), Space: 35 MB (71.65%) - LeetHub

---
+++ split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int BalancedStringSplit(string s) {
+    int coR = 0;
+       int coL = 0;
+        int k = 0;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                coL = 0;
+                coR = 0;
+                int ss = 0;
+                for (int j = i; j < s.Length; j++)
+                {
+
+                    if (s[j] == 'R')
+                    {
+
+                        coR++;
+                        ss++;
+                    }//"RLRRRLLRLL"
+                    else
+                    {
+                        coL++;
+                        ss++;
+
+                    }
+                    if (coR == coL && ss >= 2)
+                    {
+                        k++;
+                        i = j ;
+                        break;
+                    }
+                }
+                
+            }
+            return k;
+        }
+    }
