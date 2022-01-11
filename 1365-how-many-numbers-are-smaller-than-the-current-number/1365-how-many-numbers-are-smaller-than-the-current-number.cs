ad20e155267ac3dd165b8e21c8d25de81e3adfa0
MohamedAhmedAbdAlaziz <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-01-11 11:10:46 +0200

    Time: 204 ms (20.41%), Space: 40.3 MB (85.37%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cs
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cs
@@ -1,28 +1,20 @@
 public class Solution {
     public int[] SmallerNumbersThanCurrent(int[] nums) {
-            int[] ff = new int[101];
-           
+          int[] kids = new int[nums.Length];
+
             for (int i = 0; i < nums.Length; i++)
             {
-                ff[nums[i]]++;          
-            }
-
-            for (int i = 1; i < 101; i++)
-            {
-                ff[i] += ff[i-1];
-            }
-
-           
-         
-            for (int i = 0; i < nums.Length; i++)
-            {
-                int k = nums[i];
-                if (k == 0) nums[i] = 0;
-                else
+                int k = 0;
+                for (int j = 0; j < nums.Length; j++)
                 {
-                    nums[i] = ff[k - 1];
+                    if(nums[i]> nums[j])
+                    {
+                        k++;
+                    }
+
                 }
+                kids[i] = k;
             }
-            return nums;
+            return kids;
     }
 }
