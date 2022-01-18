ac29c847394b3494ce844b85da52bc9d02bb6411
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-01-18 10:43:36 +0200

    Time: 88 ms (63.48%), Space: 37.6 MB (6.09%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cs
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cs
@@ -1,8 +1,6 @@
 public class Solution {
     public int LargestAltitude(int[] gain) {
-     
-            int k = 0;
-           int max=0;
+           int k = 0,max=0;         
             for (int i = 0; i < gain.Length; i++)
             {
                 k += gain[i];
