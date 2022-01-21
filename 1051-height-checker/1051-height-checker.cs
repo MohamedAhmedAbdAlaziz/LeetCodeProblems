b63727d60d71dc8627dc8107da80748ce72c8b00
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-01-21 11:06:53 +0200

    Time: 187 ms (5.24%), Space: 36.4 MB (65.00%) - LeetHub

--- 1051-height-checker/1051-height-checker.cs
+++ 1051-height-checker/1051-height-checker.cs
@@ -1,18 +1,17 @@
 public class Solution {
     public int HeightChecker(int[] heights) {
-           int[] newarr = new int[heights.Length];
+            int[] newarr = (int[])heights.Clone();
 
-            for (int i = 0; i < heights.Length; i++)
-            {
-                newarr[i] = heights[i];
-            }
-         Array.Sort(heights);
+            Array.Sort(heights);
+
             int k = 0;
             for (int i = 0; i < heights.Length; i++)
             {
                 if (newarr[i] != heights[i])
                     k++;
             }
-            return k;   
+            return k;
+
+    
     }
 }
