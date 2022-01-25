a6768e1b1e432a89e361581109a60631d0bdef7c
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-01-25 10:21:23 +0200

    Time: 97 ms (43.90%), Space: 36.5 MB (70.73%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cs
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cs
@@ -1,25 +1,25 @@
 public class Solution {
     public bool IsSumEqual(string firstWord, string secondWord, string targetWord) {
         
-            string k = "abcdefghijklmnopqrstuvwxyz";
+        
             int t = 0, m = 0, r = 0;
             int i = 0;
             while (i < firstWord.Length || i < secondWord.Length || i < targetWord.Length)
             {
                 if (i < firstWord.Length)
                 {
-                    t = t * 10 + k.IndexOf(firstWord[i]);
+                    t = t * 10 + (firstWord[i] -97);
 
                 }
                 if (i < secondWord.Length)
                 {
-                    m = m * 10 + k.IndexOf(secondWord[i]);
+                    m = m * 10 + (secondWord[i] -97);
 
 
                 }
                 if (i < targetWord.Length)
                 {
-                    r = r * 10 + k.IndexOf(targetWord[i]);
+                    r = r * 10 +(targetWord[i] -97);
 
                 }
                 i++;
