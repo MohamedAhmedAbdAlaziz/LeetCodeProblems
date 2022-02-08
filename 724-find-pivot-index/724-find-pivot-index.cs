bffbd0cf0f84fdfb4fecb89d09f1048227e5cb47
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-02-08 09:38:30 +0200

    Time: 130 ms (58.33%), Space: 39.8 MB (57.38%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int PivotIndex(int[] nums) {
+        int t = 0;
+            int[] first = new int[nums.Length];
+            int[] second = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                t += nums[i];
+                first[i] = t;
+            }
+            t = 0;
+           for (int i = nums.Length-1; i >=0; i--) 
+            {
+                t += nums[i];
+                second[i] = t;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (first[i] == second[i])
+                    return i;
+            }
+            return -1; 
+    }
+}
