dd0cf35188fcabea5d61d72653f1afae1c335b04
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-02-16 15:54:58 +0200

    Time: 72 ms (96.07%), Space: 37.2 MB (5.68%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cs
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cs
@@ -1,27 +1,14 @@
 public class Solution {
     public string SortSentence(string s) {
-         string[] t = s.Split(" ");
-            string u = "";
-            Dictionary<int, string> prevSum = new Dictionary<int, string>();
-
-            for (int i = 0; i < t.Length; i++)
+               string[] t = s.Split(" ");
+            string[] newstring = new string[t.Length];
+             for (int i = 0; i < t.Length; i++)
             {
                 int y =Convert.ToInt32(t[i].Last().ToString());
-                prevSum[y] = t[i].Remove(t[i].Length-1);
+              
+                newstring[y - 1] = t[i].Remove(t[i].Length - 1);
             }
-            for (int i = 1; i <= t.Length; i++)
-            {
-                if(i!= t.Length)
-                {
-                    u += prevSum[i] + " ";
-
-                }
-                else
-                {
-                    u += prevSum[i] ;
-
-                }
-            }
-            return u;
+         
+            return String.Join(" ", newstring) ;
     }
 }
