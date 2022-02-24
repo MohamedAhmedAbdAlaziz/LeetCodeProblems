f6f892adde20effa51c75ba123c5655482c944c2
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-02-24 17:32:37 +0200

    Time: 135 ms (64.91%), Space: 42.7 MB (33.92%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cs
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cs
@@ -4,16 +4,11 @@ public class Solution {
             for (int i = 0; i < chars.Length; i++)
             {
                 if (prevSum.ContainsKey(chars[i]))
-                {
                     prevSum[chars[i]] = 1 + prevSum[chars[i]];
-                }
                 else
-                {
                     prevSum[chars[i]] = 1;
-                }
             }
             int num = 0;
-
             for (int i = 0; i < words.Length; i++)
             {
                 int j = 0;
@@ -21,19 +16,13 @@ public class Solution {
                  for (j = 0; j < words[i].Length; j++)
                 {
                     if (prevSu.ContainsKey(words[i][j]) && prevSu[words[i][j]]>0)
-                    {
                         prevSu[words[i][j]] = prevSu[words[i][j]] - 1;
-                      //  break;
-                    }
                     else
-                    {
                         break;
-                    }
                }
-                if (j == words[i].Length)
-                {
+                if (j == words[i].Length)    
                     num+= words[i].Length;
-                }
+            
             }
             return num;
     }
