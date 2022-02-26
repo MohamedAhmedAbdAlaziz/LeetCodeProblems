db5170266f017285f2ec4c86f5e4e730ceacabbc
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-02-26 18:42:31 +0200

    Time: 392 ms (52.94%), Space: 37.6 MB (17.65%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public int RepeatedStringMatch(string a, string b) {
+            int t = 1;
+            string s = new string(a);
+
+            while (s.Length< b.Length)
+            {
+                s += a;
+                t++;
+            }
+            int u=0;
+            char bs = b[0];
+           if (s.IndexOf(b[0]) == -1)
+            {
+                return -1;
+            }
+            int n = (int)Math.Ceiling((double)b.Length / (double)a.Length);
+            int tn = n - 1;
+        
+            while (tn<= n)
+            {
+               
+                for (int i = s.IndexOf(b[0]) ; i <= s.Length-b.Length; i++)
+                {
+
+                    if (s[i] == bs && (s.Length-i+1>=b.Length))
+                    {
+                       if( s.Substring(i, b.Length)==b)
+                        {
+                            return t;
+                        }
+                    }
+                }
+                s += a;
+                t++;
+                tn++;
+            }
+            return -1;
+    }
+}
