8ebe0904a1fe2ac4424403ca616706c64fba9344
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-02-28 17:04:54 +0200

    Time: 101 ms (76.72%), Space: 37.5 MB (70.23%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cs
+++ 137-single-number-ii/137-single-number-ii.cs
@@ -1,21 +1,12 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-                     Dictionary<int, int> prevSu = new Dictionary<int, int>();
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (!prevSu.ContainsKey(nums[i]))
-                {
-                    prevSu[nums[i]] = 1;
-
-                }
-                else
-                {
-                    prevSu[nums[i]] = prevSu[nums[i]] + 1;
-                }
-            }
-   
-
-            return prevSu.Where(x => x.Value == 1).Select(x => x.Key).FirstOrDefault();
+                     int p = 0;  
+              int q = 0;  
+           for(int i = 0; i<nums.Length; i++){  
+              p = q & (p ^ nums[i]);  
+              q = p | (q ^ nums[i]);  
+           }  
+           return q; 
        
     }
 }
