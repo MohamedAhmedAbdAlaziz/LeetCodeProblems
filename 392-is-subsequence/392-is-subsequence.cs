45108932f81cd0311959adf1efe63ecd650c8ac0
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-03 01:37:40 +0200

    Time: 78 ms (81.55%), Space: 36.3 MB (62.86%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cs
+++ 392-is-subsequence/392-is-subsequence.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public bool IsSubsequence(string s, string t) {
-         int i = 0,j=0;
+        int i = 0,j=0;
             while (  i < s.Length && j< t.Length )
             {
                 if (s[i] == t[j])
