e3a8d2e36118170f70236a3d019d6446aa0c33b2
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-02 11:26:39 +0200

    Time: 152 ms (23.73%), Space: 36.4 MB (55.93%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public string RemoveOuterParentheses(string s) {
+             StringBuilder S = new StringBuilder();
+            Stack<char> st = new Stack<char>();
+     
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if (s[i] == '(' )
+                {
+                    if (st.Count > 0)
+                        S.Append(s[i]);
+
+                  st.Push(s[i]);
+
+                  
+                   
+                }
+                else
+                {
+                    st.Pop();
+                    if (st.Count > 0)
+                        S.Append(s[i]);
+                }
+                
+                             
+                 
+            }
+            return S.ToString();  
+    }
+}
