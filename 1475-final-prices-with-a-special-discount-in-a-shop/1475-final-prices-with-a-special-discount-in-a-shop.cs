61f5bc6566cc7d339c0edd668dca21f9d9548e7c
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-02 23:20:07 +0200

    Time: 189 ms (55.67%), Space: 42.4 MB (16.49%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] FinalPrices(int[] prices) {
+          Stack<int> st = new Stack<int>();
+         
+            for (int i = 0; i < prices.Length; i++)
+            {
+               
+                while(st.Count>0 && prices[st.Peek()]>=prices[i])
+                {
+                    prices[st.Pop()] -= prices[i]; 
+                }
+
+                st.Push(i);
+                
+            }
+            return prices;
+    }
+}
