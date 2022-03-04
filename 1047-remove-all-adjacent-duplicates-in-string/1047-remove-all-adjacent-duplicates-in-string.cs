9fb306ea15d101001c837565dfa7de343f866c43
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-04 10:22:57 +0200

    Time: 1558 ms (9.94%), Space: 42.9 MB (6.55%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cs
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cs
@@ -1,24 +1,15 @@
 public class Solution {
     public string RemoveDuplicates(string s) {
         Stack<char> stack = new Stack<char>();
-           
             for (int i = 0; i < s.Length; i++)
             {
-
-                if (stack.Count > 0 && stack.Peek() == s[i])
-                {
+               if (stack.Count > 0 && stack.Peek() == s[i])
                    stack.Pop();
-                }
                 else
-                {
-                    stack.Push(s[i]);
-
-                }
-
-            }
+                 stack.Push(s[i]);   
+                  }
             string t = "";
-           
-            while (stack.Count()>0)
+              while (stack.Count()>0)
             {
                 t = stack.Pop() + t;
             }
