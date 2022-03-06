6c7f7600a3a33816e7b9d7ed6cc35d091a5ae774
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-06 10:12:30 +0200

    Time: 158 ms (6.08%), Space: 36.6 MB (55.53%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsValid(string s) {
+       Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (stack.Count() > 0 && 
+                    (stack.Peek()== s[i]-2 ||
+                    stack.Peek() == s[i] - 1
+                    ) )
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(s[i]);
+
+                }
+
+            }
+            if (stack.Count() > 0)
+            {
+                return false;
+            }
+            return true;
+    }
+}
