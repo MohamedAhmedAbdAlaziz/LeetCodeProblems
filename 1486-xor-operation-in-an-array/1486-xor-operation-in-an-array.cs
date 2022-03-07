32ac7f02570f3904248b8ff21c29d1fff2697248
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-07 18:41:17 +0200

    Time: 26 ms (79.17%), Space: 25.4 MB (51.04%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cs
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cs
@@ -1,10 +1,8 @@
 public class Solution {
     public int XorOperation(int n, int start) {
         int t2 = 0;
-
-            for (int i = 0; i < n; i++)
-            {   t2 ^= start + 2 * i;
-            }
+          for (int i = 0; i < n; i++) t2 ^= start + 2 * i;
+            
             return t2;
     }
 }
