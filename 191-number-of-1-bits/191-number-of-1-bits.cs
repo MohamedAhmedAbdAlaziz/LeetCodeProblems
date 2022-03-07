56782442b9a27438760cf00fd60f7fa845b35f1c
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-07 16:43:10 +0200

    Time: 20 ms (93.41%), Space: 23.7 MB (45.25%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cs
+++ 191-number-of-1-bits/191-number-of-1-bits.cs
@@ -1,10 +1,9 @@
 public class Solution {
     public int HammingWeight(uint n) {
-         int t = 0;
+            int t = 0;
             while (n > 0)
-            {
-                t++;
-                n = n & (n - 1);
+            {   t++;
+            n = n & (n - 1);
             }
             return t;  
     }
