6e12d80102ca26a0afacc6c174dfc73e19995c14
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-09 11:09:57 +0200

    Time: 30 ms (88.89%), Space: 25.2 MB (77.78%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cs
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cs
@@ -2,22 +2,16 @@ public class Solution {
     public int MinFlips(int a, int b, int c) {
            int Count = 0;
             for (int i = 0; i < 32; i++)
-            {
-                int t = (a % 2);
-                int t2 = (b % 2);
-                 int y = t | t2;
+            { 
+                int y = (a % 2) | (b % 2);
                 int tc = c % 2;
                 if (y != tc)
-                {
-                    Count++;
-                   
-                    if (t==1 && t2==1)
-                    {
-                        Count++;
-
-                    }
+                {  
+                    Count++; 
+                    if ((a % 2) == 1 && (b % 2) == 1)
+                        Count++;      
                 }
-                
+
                 a = a >> 1;
                 b = b >> 1;
                 c = c >> 1;
