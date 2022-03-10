3ac5917c3c569581c4d08f2767d3a644f43f481c
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-11 01:07:52 +0200

    Time: 140 ms (85.51%), Space: 46.7 MB (10.14%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cs
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cs
@@ -4,24 +4,17 @@ Dictionary<int, IList<int>> dic = new Dictionary<int, IList<int>>();
              IList<IList<int>> list = new List<IList<int>>();
             for (int i = 0; i < groupSizes.Length; i++)
             {
-                int w = groupSizes[i];
-               if(dic.ContainsKey(w))
-                {
-                    dic[w].Add(i);
-                }    
+                 
+               if(dic.ContainsKey(groupSizes[i]))
+                    dic[groupSizes[i]].Add(i); 
                 else
-                {
-                    dic[w] = new List<int> { i };
-                }
+                    dic[groupSizes[i]] = new List<int> { i };
             }
             foreach (var item in dic)
             {
                 for (int i = 0; i < item.Value.Count(); i+=item.Key)
-                {
-                    var t = item.Value.Skip(i).Take(item.Key).ToList();
-                    list.Add(t);
-                }
-            }
+                  list.Add( item.Value.Skip(i).Take(item.Key).ToList());
+                  }
 
             return list;
     }
