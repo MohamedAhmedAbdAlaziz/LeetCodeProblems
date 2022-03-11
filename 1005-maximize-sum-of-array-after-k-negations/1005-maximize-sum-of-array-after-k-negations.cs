9aa9a818f00b783033b8b7aa8c7d2565bcdc8cea
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-11 23:40:24 +0200

    Time: 123 ms (69.23%), Space: 38 MB (25.00%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cs
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cs
@@ -5,26 +5,19 @@ public class Solution {
             int i = 0;
             int j = 0;
             while ( j < k)
-            {
-
-                if (i >= nums.Length) {
-                    nums[nums.Length-1] = -1 * nums[nums.Length - 1];
-                     
-                }
-                else if (i > 0 && nums[i] > nums[i - 1])
-                {
+            { 
+            if (i >= nums.Length)  
+              nums[nums.Length-1] = -1 * nums[nums.Length - 1];                
+        
+             else if (i > 0 && nums[i] > nums[i - 1])
                      nums[i - 1] = -1 * nums[i - 1];
-                }
                 else
                 {
                     nums[i] = -1 * nums[i];
-
                     i++;
-
-                     ;
                 }
                 j++;
-                   }
+             }
 
             return nums.Sum();
     }
