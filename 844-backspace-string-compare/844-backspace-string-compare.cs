b5c8169e3daee4fb2c4dcc0275131d5f1a993e8b
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-12 16:27:06 +0200

    Time: 160 ms (5.62%), Space: 36.8 MB (60.03%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cs
+++ 844-backspace-string-compare/844-backspace-string-compare.cs
@@ -2,31 +2,22 @@ public class Solution {
     public bool BackspaceCompare(string s, string t)
     { 
       Stack<char> stack = new Stack<char>();
-            Stack<char> stack2 = new Stack<char>();
-
-            for (int i = 0; i < s.Length; i++)
+      Stack<char> stack2 = new Stack<char>(); 
+        
+        for (int i = 0; i < s.Length; i++)
             {
 
-              if(stack.Count() >0 && s[i]=='#')
-                {
-                    stack.Pop();
-                }
+              if(stack.Count() >0 && s[i]=='#')  stack.Pop();
                 else if(s[i] != '#')
-                {
                     stack.Push(s[i]);
-                }
             }
             for (int i = 0; i < t.Length; i++)
             {
 
                 if (stack2.Count() > 0 && t[i] == '#')
-                {
                     stack2.Pop();
-                }
                 else if(t[i] != '#')
-                {
                     stack2.Push(t[i]);
-                }
             }
             string t4 = new String(stack.ToArray());
             string t42 = new String(stack2.ToArray());
