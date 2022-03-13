db4da7c3d35fecbcadb833c5bec6bdf687050870
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-13 20:06:17 +0200

    Time: 120 ms (29.82%), Space: 37.1 MB (32.46%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.cs
+++ 925-long-pressed-name/925-long-pressed-name.cs
@@ -1,39 +1,25 @@
 public class Solution {
     public bool IsLongPressedName(string name, string typed) {
-            
-           int j = 0;
+          int j = 0;
             int i = 0;
+            int tt = 0;
             for (  i = 0; i < name.Length; i++)
-            {
-
-                if (i == name.Length - 1 || name[i] != name[i + 1])
-                {
-                    int tt = 0;
+            { if (i == name.Length - 1 || name[i] != name[i + 1])
+                { 
+                    tt = 0;
                     while (j <= typed.Length - 1 && name[i] == typed[j])
-                    {
-                        j++;
+                    {   j++;
                         tt++;
                     }
                     if (tt == 0 )
-                    {
                         return false;
-                    }
                 }
                 else
-                {
-                    j++;
+                   j++;  
+             }
+             
+            if (j <= typed.Length - 1 && typed[j] != name[name.Length - 1]) return false;
 
-                }
-
-            }
-
-            if(j<=typed.Length - 1)
-            {
-                if (typed[j] != name[name.Length - 1])
-                {
-                    return false;
-                }
-            }
-            return true;
+          return true;
     }
 }
