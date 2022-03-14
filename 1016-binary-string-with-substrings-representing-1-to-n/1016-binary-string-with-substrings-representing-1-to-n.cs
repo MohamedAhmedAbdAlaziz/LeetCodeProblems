2f29876b2de8d0c638cda1b31768706fb83ab9c8
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-14 08:49:38 +0200

    Time: 97 ms (69.23%), Space: 36.6 MB (38.46%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cs
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cs
@@ -2,12 +2,9 @@ public class Solution {
     public bool QueryString(string s, int n) {
           int i = 1;
             while(i<=n)
-            {
-            
-                if (!s.Contains(Convert.ToString(i, 2))) {
+            {  if (!s.Contains(Convert.ToString(i, 2))) 
                     return false;
-                }
-                i++;
+                  i++;
             }
 
             return true;
