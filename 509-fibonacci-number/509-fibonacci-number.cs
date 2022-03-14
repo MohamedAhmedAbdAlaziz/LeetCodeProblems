b7582e1789dd28fa050187be881d6088b152e9b9
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-14 18:45:23 +0200

    Time: 53 ms (21.82%), Space: 24.8 MB (99.16%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cs
+++ 509-fibonacci-number/509-fibonacci-number.cs
@@ -1,10 +1,8 @@
 public class Solution {
     public int Fib(int n) {
         if(n<=1)return n;
-        
-        int a= Fib(n-1);
-        int b= Fib(n-2);
-        return a+b;
+       return  Fib(n-1)+Fib(n-2);
+    
 
         
     }
