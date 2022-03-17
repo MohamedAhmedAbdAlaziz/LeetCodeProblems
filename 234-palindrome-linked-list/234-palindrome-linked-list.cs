c56aaab6309686ade24369f036269eb1fea6f526
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-17 10:01:55 +0200

    Time: 356 ms (58.49%), Space: 53.4 MB (46.50%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cs
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+      ListNode fast = head;
+            ListNode slow = head;
+            while(fast !=null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            ListNode prev = null;
+            while(slow!= null)
+            {
+                ListNode temp = slow.next;
+                slow.next = prev;
+                prev = slow;
+                slow = temp;
+            }
+            ListNode left = head;
+            ListNode right = prev;
+
+            while(right != null)
+            {
+                if(left.val != right.val)
+                {
+                    return false;
+                }
+                left = left.next;
+                right = right.next;
+            }
+
+            return true;
+    }
+}
