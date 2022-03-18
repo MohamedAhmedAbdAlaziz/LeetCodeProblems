bd19b9feeb4600c17b23488d1bafaf1d25220203
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-18 12:19:44 +0200

    Time: 153 ms (21.67%), Space: 38.1 MB (28.05%) - LeetHub

--- 415-add-strings/415-add-strings.cs
+++ 415-add-strings/415-add-strings.cs
@@ -5,29 +5,23 @@ public class Solution {
             int j = num2.Length-1;
             string result = "";
             while (i>=0 || j>=0)
-            {
-                int t1 = 0;
+            {   int t1 = 0;
                 int t2 = 0;
                 if (i>=0)
                t1=  Convert.ToInt32(num1[i].ToString());
                if(j>=0)
                  t2=  Convert.ToInt32(num2[j].ToString());
-
                 int re = t1 + t2 + rest;
                 if(re / 10 == 1)
                 {
-                    result = (re % 10) + result; 
-
-                       rest = re /10;
+                     result = (re % 10) + result; 
+                     rest = re /10;
                 }
                 else
-                {
-                    result = (re ) + result; 
+                {  result = re + result; 
                   rest = 0;
                 }
-                 
-               
-                if (i >= 0)
+                  if (i >= 0)
                     i--;
                 if (j >= 0)
                     j--;
