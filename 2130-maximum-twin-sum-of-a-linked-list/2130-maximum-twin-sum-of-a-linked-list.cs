a6d83d0771df0a5f2d216989fe84ea06ef4bbcd1
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-18 19:26:51 +0200

    Time: 280 ms (91.78%), Space: 54.5 MB (17.81%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public int PairSum(ListNode head) {
+         ListNode p = head;
+            ListNode p2 = head;
+            Stack<int> stack = new Stack<int>();
+            int i = 0;
+            int t = 0;
+            while (p != null)
+            {
+
+                if(p2== null)
+                {
+                    t = Math.Max(t, p.val + stack.Pop());
+                }
+                else
+                {
+                    stack.Push(p.val);
+
+                }
+
+                p = p.next;
+              if(p2 != null)
+                p2 = p2.next.next;
+            }
+
+            return t;
+    }
+}
