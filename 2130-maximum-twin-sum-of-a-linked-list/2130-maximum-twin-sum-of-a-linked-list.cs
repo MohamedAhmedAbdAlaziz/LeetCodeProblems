6a8ba1643ca1543a3ecda5b39442f3fef0886723
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-18 19:27:43 +0200

    Time: 497 ms (23.75%), Space: 54.9 MB (13.24%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cs
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cs
@@ -14,21 +14,12 @@ public class Solution {
          ListNode p = head;
             ListNode p2 = head;
             Stack<int> stack = new Stack<int>();
-            int i = 0;
             int t = 0;
             while (p != null)
-            {
-
-                if(p2== null)
-                {
-                    t = Math.Max(t, p.val + stack.Pop());
-                }
-                else
-                {
-                    stack.Push(p.val);
-
-                }
-
+            {   
+                if(p2== null) t = Math.Max(t, p.val + stack.Pop());
+                
+                else    stack.Push(p.val);
                 p = p.next;
               if(p2 != null)
                 p2 = p2.next.next;
