1fddcf792d418db3c974e4fa31deafb79100779f
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-21 20:24:50 +0200

    Time: 151 ms (10.01%), Space: 36.7 MB (75.68%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode SwapPairs(ListNode head) {
+       ListNode d = new ListNode(0, head);
+
+            ListNode pre = d;
+            ListNode curr = head;
+            while(curr != null && curr.next != null)
+            {
+                ListNode nxtpair = curr.next.next;
+                ListNode seconnd = curr.next;
+                seconnd.next = curr;
+                curr.next = nxtpair;
+                pre.next = seconnd;
+
+                pre = curr;
+                curr = nxtpair;
+
+            }
+            return d.next;   
+    }
+}
