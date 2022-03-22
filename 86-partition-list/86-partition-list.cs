b585b675e9f538a65cad57384ced9d8e0d1e9bc9
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-22 14:39:18 +0200

    Time: 76 ms (97.24%), Space: 39.3 MB (6.21%) - LeetHub

---
+++ 86-partition-list/86-partition-list.cs
@@ -0,0 +1,50 @@
+public class Solution {
+    public ListNode Partition(ListNode head, int x) {
+                 if(head== null)  return head; 
+                if(head.next== null)  return head;   
+
+        ListNode p =head;
+            ListNode left =new(0, null);
+            ListNode te =  left;
+            ListNode right = new(0, null);
+            ListNode te2 = right;
+             
+            while (p!= null)
+            {
+                if (p.val < x) {
+                    ListNode temp = p.next;
+                       
+                        p.next = null;
+                        te.next = p;
+                        te = te.next;
+  
+                        
+                    p = temp;
+
+                }
+                else
+                {
+                    ListNode temp = p.next;
+
+                    p.next = null;
+                    te2.next = p;
+                    te2 = te2.next;
+                    p = temp;
+                }
+
+            }
+          left = left.next;
+            right = right.next;
+
+            if (left == null)
+            {
+                return right;
+            }
+            te.next = right;
+            te = te.next;
+           
+        
+            return left;
+
+    }
+}
