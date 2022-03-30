f773dc70076426c0a344e4cd522bb646ecf3c69d
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-03-30 17:15:16 +0200

    Time: 109 ms (40.97%), Space: 36.8 MB (42.36%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.cs
+++ 551-student-attendance-record-i/551-student-attendance-record-i.cs
@@ -2,14 +2,11 @@ public class Solution {
     public bool CheckRecord(string s) {
        if (s.Contains("LLL")) return false;
             int t = 0;
-            
             for (int i = 0; i < s.Length; i++)
-            {
-                if(s[i]=='A')
+            {  if(s[i]=='A')
                 t++;
                 if (t == 2) return false; 
             }
-
             return true;   
     }
 }
