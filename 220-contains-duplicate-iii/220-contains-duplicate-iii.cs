8bab2fa6fa8236afa30b2256893ca8e5852a52d5
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-04-01 11:24:58 +0200

    Time: 600 ms (5.00%), Space: 43.9 MB (81.25%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cs
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cs
@@ -4,15 +4,9 @@ public class Solution {
             {
                 int j = i+1;
                 while (j<i+k+1  && j <nums.Length)
-                {
-                      //  int mt = 1;
-
-                        if (Math.Abs((double)nums[j] - (double)nums[i]) <= t)
-                    {
-                        return true;
-                    }
-                 //   mt++;
-                    j++;
+                {  if (Math.Abs((double)nums[j] - (double)nums[i]) <= t)
+                      return true;
+                 j++;
                 }
             }
             return false;
