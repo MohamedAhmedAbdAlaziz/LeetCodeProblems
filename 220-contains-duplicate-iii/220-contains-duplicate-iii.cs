1acd57f1f8989a4c13c653b38fefb6944f24e36a
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-04-01 11:44:12 +0200

    Time: 142 ms (85.00%), Space: 43.6 MB (91.25%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cs
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cs
@@ -1,14 +1,22 @@
 public class Solution {
     public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-           for (int i = 0; i < nums.Length; i++)
-            {
-                int j = i+1;
-                while (j<i+k+1  && j <nums.Length)
-                {  if (Math.Abs((double)nums[j] - (double)nums[i]) <= t)
-                      return true;
-                 j++;
+           var buckets = new Dictionary<long, long>();
+            if (t < 0) return false;
+            for (int i = 0; i < nums.Length; i++)             
+            {                 
+                var bucketIndex = ((long) nums[i] - int.MinValue)/((long) t + 1);                 
+                if (i - k - 1 >= 0)
+                {
+                    buckets.Remove(((long)nums[i - k - 1] - int.MinValue) / ((long)t + 1));
                 }
+
+                if (buckets.ContainsKey(bucketIndex)
+                    || buckets.ContainsKey(bucketIndex - 1) && nums[i] - buckets[bucketIndex - 1] <= t
+                    || buckets.ContainsKey(bucketIndex + 1) && buckets[bucketIndex + 1] - nums[i] <= t)
+                    return true;
+                buckets[bucketIndex] = nums[i];
             }
+
             return false;
     }
 }
