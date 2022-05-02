352665072ff5c876852721266971887098558645
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-02 19:25:10 +0200

    Time: 202 ms (37.58%), Space: 41.7 MB (30.86%) - LeetHub

--- 48-rotate-image/48-rotate-image.cs
+++ 48-rotate-image/48-rotate-image.cs
@@ -3,21 +3,16 @@ public class Solution {
          List<List<int>> list = new List<List<int>>();
             for (int i = 0; i < matrix[0].Length; i++)
             {
-                List<int> l = new List<int>();
-                    for (int j = 0; j < matrix.Length; j++)
-                    {
-                    l.Insert(0,matrix[j][i]);
-                    }
-                list.Add(l);
+              List<int> l = new List<int>();
+                   for (int j = 0; j < matrix.Length; j++)
+                     l.Insert(0,matrix[j][i]);
+                   list.Add(l);
                 
-            }
+                 }
 
             for (int i = 0; i < matrix[0].Length; i++)
-            {
-                 
-                
-                    matrix[i]= list[i].ToArray();
-                }
+                  matrix[i]= list[i].ToArray();
+         
               
 
     }
