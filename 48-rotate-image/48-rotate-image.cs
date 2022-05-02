c145309eb4c7670402cf11e876c369c03efb06a3
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-02 19:23:46 +0200

    Time: 197 ms (41.56%), Space: 41.2 MB (74.06%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+         List<List<int>> list = new List<List<int>>();
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                List<int> l = new List<int>();
+                    for (int j = 0; j < matrix.Length; j++)
+                    {
+                    l.Insert(0,matrix[j][i]);
+                    }
+                list.Add(l);
+                
+            }
+
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                 
+                
+                    matrix[i]= list[i].ToArray();
+                }
+              
+
+    }
+}
