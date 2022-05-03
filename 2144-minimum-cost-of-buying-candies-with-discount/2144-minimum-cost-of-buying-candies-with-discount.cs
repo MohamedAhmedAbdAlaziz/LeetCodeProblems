f5d5e6a2b4c17cb2d6576998416af7a4895df229
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-03 10:38:53 +0200

    Time: 105 ms (63.64%), Space: 37.7 MB (51.52%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cs
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cs
@@ -1,11 +1,9 @@
 public class Solution {
     public int MinimumCost(int[] cost) {
-      
-            if (cost.Length == 1) return cost[0];
-            if (cost.Length == 2) return cost[0]+ cost[1] ;
-        Array.Sort(cost);
-        Array.Reverse(cost);
-
+           if (cost.Length == 1) return cost[0];
+           if (cost.Length == 2) return cost[0]+ cost[1] ;
+            Array.Sort(cost);
+            Array.Reverse(cost);
             int sum = 0;
            int i = 0;
             while ( i < cost.Length )
