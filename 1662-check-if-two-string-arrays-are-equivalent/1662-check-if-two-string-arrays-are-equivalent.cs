c0170e01ebe4d9229510805fd8168cae5e141842
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-04 20:35:19 +0200

    Time: 124 ms (40.80%), Space: 39.5 MB (62.07%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cs
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cs
@@ -1,13 +1,13 @@
 public class Solution {
     public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
-        string s1 = "";
+         string s1 = "";
             string s2 = "";
-            for (int i = 0; i < word1.Length; i++)
-              s1 += word1[i];
-             
-            for (int i = 0; i < word2.Length; i++)
-              s2 += word2[i];
-           
+            int t = Math.Max(word2.Length, word1.Length);
+            for (int i = 0; i < t; i++)
+            {
+              if(i< word1.Length) s1 += word1[i];
+              if(i< word2.Length)s2 += word2[i];
+            }
 
             return s2 == s1;
     }
