8b5d2b422428d85eeab630bec61195fd269059f4
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-04 22:26:31 +0200

    Time: 126 ms (22.41%), Space: 37 MB (31.12%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        int t = Math.Max(word2.Length, word1.Length);
+      
             string s = "";
-            for (int i = 0; i < t; i++)
+            for (int i = 0; i < Math.Max(word2.Length, word1.Length); i++)
             {
                 if (i < word1.Length) s += word1[i];
                 if (i < word2.Length) s += word2[i];
