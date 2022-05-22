e3a75641b7b401b9134fc12746803152cd50d3f3
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-22 09:19:04 +0200

    Time: 304 ms (25.79%), Space: 49.8 MB (39.37%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public IList<int> FindDuplicates(int[] nums) {
+         var d = new Dictionary<int, int>();
+            List<int> t = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (d.ContainsKey(nums[i]))
+                {
+                    if(d[nums[i]] == 1)
+                       t.Add(nums[i]);
+                    d[nums[i]] += 1;
+                }   
+                else { 
+                    d[nums[i]] = 1;
+                }
+            }
+            return t;
+    }
+}
