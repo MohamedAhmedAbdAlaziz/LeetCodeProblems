4e14e7745e668a846e2cdc383130e526ef0d2b12
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-22 14:25:47 +0200

    Time: 156 ms (86.11%), Space: 44.9 MB (8.33%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cs
@@ -0,0 +1,64 @@
+public class Solution {
+    public IList<string> FindAndReplacePattern(string[] words, string pattern) {
+         var dic = new Dictionary<char, string >();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+
+                if (dic.ContainsKey(pattern[i]))
+                {
+
+                   dic[pattern[i]] = dic[pattern[i]] + "" + i;
+                }
+                else
+                {
+                    dic[pattern[i]] =  i+"" ;
+
+                }
+            }
+
+          //  var dd = dic.SelectMany(x => x.Value).ToList().Select(x=>x.ToString()).Aggregate((f, s) => f + "" + s);
+            var dd22 = dic.Select(x => x.Value).ToList();
+            //      
+            List<string> list = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                var dic1 = new Dictionary<char, string>();
+
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    if (dic1.ContainsKey(words[i][j]))
+                    {
+
+                        dic1[words[i][j]]= dic1[words[i][j]]+"" +j;
+                    }
+                    else
+                    {
+                        dic1[words[i][j]]= j+"";
+
+                    }
+                }
+               // string tyu = dic1.SelectMany(x => x.Value).ToList().Select(x => x.ToString()).Aggregate((f, s) => f + "" + s);
+                var tyu2 = dic1.Select(x => x.Value).ToList();
+               // if (dic1.SelectMany(x => x.Value).ToList().Select(x => x.ToString()).Aggregate((f, s) => f + "" + s) == dd)
+               //{
+               //     list.Add(words[i]);
+               //}
+                if(tyu2.Count()!= dd22.Count()) continue;
+                int k = 0;
+                for (k= 0; k <tyu2.Count(); k++)
+                {
+                    if(tyu2[k] != dd22[k] )
+                    {
+                        break ;
+                    }
+                    
+                }
+                if(k==tyu2.Count())
+                list.Add(words[i]);
+
+
+            }
+            return list;
+    }
+}
