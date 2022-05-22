d4355184d874c944d901e37871f51004e224c26b
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-22 17:56:32 +0200

    Time: 298 ms (44.44%), Space: 54.6 MB (55.55%) - LeetHub

---
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public int FindLeastNumOfUniqueInts(int[] arr, int k) {
+       
+                var dic1 = new Dictionary<int, int>();
+           
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dic1.ContainsKey(arr[i]))
+                {
+                    dic1[arr[i]] += 1; 
+                }
+                else
+                {
+                    dic1[arr[i]] = 1;
+
+                }
+            }
+
+       //  var rr= dic1.OrderBy(x => x.Value);
+            var rr = dic1.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            int t = 0;
+            foreach (var item in rr)
+            {
+                int gg = item.Key;
+                int yy = rr[gg];
+                if (k < yy) return rr.Skip(t).Count();
+                else if (k==yy) return rr.Skip(t+1).Count();
+                else
+                {
+                    k = k - yy;
+                    t++;
+                }
+              
+                
+
+            }
+
+            return rr.Skip(t).Count();
+           
+    }
+}
