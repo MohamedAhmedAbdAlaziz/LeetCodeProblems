455ada6545b25300a67ca072665873a985bc5d2c
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-23 13:21:06 +0200

    Time: 209 ms (26.67%), Space: 43.2 MB (33.33%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cs
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cs
@@ -4,17 +4,13 @@ public class Solution {
             var dic2 = new Dictionary<char, int>();
 
             for (int i = 0; i < word1.Length; i++)
-            {
-
-                if (dic.ContainsKey(word1[i]))
+            { if (dic.ContainsKey(word1[i]))
                 {
                     dic[word1[i]] += 1;
                 }
                 else
                 {
                     dic[word1[i]] = 1;
-
-
                 }
             }
             for (int i = 0; i < word2.Length; i++)
@@ -33,10 +29,9 @@ public class Solution {
                 }
             }
 
-            var tt = dic.OrderBy(x => x.Value).ToDictionary(x => x.Key, y => y.Value).Select(x=>x.Value).ToList();
-            var tt2 = dic2.OrderBy(x => x.Value).ToDictionary(x => x.Key, y => y.Value).Select(x=>x.Value).ToList();
-             
-            if (dic.Count() != dic2.Count()) return false;
+            var tt = dic.OrderBy(x => x.Value).Select(x=>x.Value).ToList();
+            var tt2 = dic2.OrderBy(x => x.Value).Select(x=>x.Value).ToList();
+              if (dic.Count() != dic2.Count()) return false;
 
             for (int i = 0; i < tt.Count(); i++)
             {
