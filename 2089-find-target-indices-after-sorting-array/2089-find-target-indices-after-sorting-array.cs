f667c847a316793ac77d6975ea7b5ec358acc711
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-23 21:34:19 +0200

    Time: 151 ms (83.08%), Space: 44.1 MB (5.47%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cs
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cs
@@ -1,8 +1,6 @@
 public class Solution {
     public IList<int> TargetIndices(int[] nums, int target) {
-          //Array.Sort(nums);
-         //   List<int> list = new List<int>();
-            return nums.OrderBy(x=>x).Select((x,y) => new   { 
+           return nums.OrderBy(x=>x).Select((x,y) => new   { 
             itemName = x,
             postion = y
             }).Where(x => x.itemName == target).Select(x=>x.postion).ToList();
