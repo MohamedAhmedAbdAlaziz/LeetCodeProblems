c633676af1d5b352c044436fd3dff9a20bfad3ca
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-25 17:05:30 +0200

    Time: 84 ms (44.87%), Space: 35 MB (46.88%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cs
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cs
@@ -2,16 +2,9 @@ public class Solution {
     public int MinAddToMakeValid(string s) {
          Stack<int> stack = new Stack<int>();
             for (int i = 0; i < s.Length; i++)
-            {
-                if (stack.Count() > 0 && stack.Peek() == '(' && s[i] == ')')
-                {
-                   stack.Pop();
+            {  if (stack.Count() > 0 && stack.Peek() == '(' && s[i] == ')')    stack.Pop();
+                 else  stack.Push(s[i]);
                 }
-                else
-                {
-                    stack.Push(s[i]);
-                }
-            }
 
             return stack.Count();
     }
