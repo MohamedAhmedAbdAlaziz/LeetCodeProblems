20a47f81ea43529d67acdbceb8a1479ab5ba780f
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-25 19:03:16 +0200

    Time: 276 ms (5.56%), Space: 69 MB (54.44%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cs
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cs
@@ -4,17 +4,12 @@ public class Solution {
             for (int i = 0; i < s.Length; i++)
             {
                 if (stack.Count() > 0 && s[stack.Peek()] == '[' && s[i] == ']')
-                {
                     stack.Pop();
-                }
-                else
-                {
-                    stack.Push(i);
-                }
-            }
-          if (stack.Count() == 0) return 0; 
-            if (stack.Count() == 2) return 1; 
-            if (stack.Count() % 4 == 0) return stack.Count() / 4;
+                 else   stack.Push(i);
+               }
+             if (stack.Count() == 0) return 0; 
+               if (stack.Count() == 2) return 1; 
+              if (stack.Count() % 4 == 0) return stack.Count() / 4;
             
                  return(stack.Count()+2) / 4; 
              
