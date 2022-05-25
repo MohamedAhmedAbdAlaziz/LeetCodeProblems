8e55d68d0834f961277eb88336cd378211d54351
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-05-25 20:08:32 +0200

    Time: 204 ms (11.39%), Space: 41.2 MB (60.00%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
@@ -5,23 +5,16 @@ public class Solution {
 
             for (  i = 0; i < s.Length; i++)
             {
-                if (stack.Count() > 0 && s[stack.Peek()] == '(' && s[i] == ')')
-                {
+                if (stack.Count() > 0 && s[stack.Peek()] == '(' && s[i] == ')') 
                     stack.Pop();
-                }
-                else if(s[i]== '(' || s[i] == ')')
-                {
-                    stack.Push(i);
-                }
+             
+                else if(s[i]== '(' || s[i] == ')')    stack.Push(i);
+               
             }
             if (stack.Count() == 0) return s;
              
-            while (stack.Count() != 0)
-            {
+            while (stack.Count() != 0) 
                 s = s.Remove(stack.Pop(), 1);
-               
-            }
-            //var tttt = s.Where(x => x.ToString() != "(" || x.ToString() != ")").ToList();
-            return s ;
+                  return s ;
     }
 }
