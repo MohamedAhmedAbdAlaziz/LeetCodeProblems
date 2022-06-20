ab1a0284b6aa863d7586cce87c3939931e5634a6
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-06-21 01:12:35 +0200

    Time: 197 ms (5.56%), Space: 37.9 MB (97.78%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cs
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public string ReversePrefix(string word, char ch) {
         int t = word.IndexOf(ch);
-            string s = word.Substring(0, t+1);
-            string s1 = "";
-            for (int i= s.Length-1; i>=0 ; i--)
-            {
-                s1 += s[i];
-            }
-           return s1 += word.Substring(t+1);
+            char[] s = word.Substring(0, t+1).ToCharArray();
+                Array.Reverse(s);
+            
+             
+           return  string.Join("",s)+ word.Substring(t+1);
+             
     }
 }
