b03996873fe2d00ae7d44f6fd80fe9f0a19cc5c2
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-06-21 21:05:38 +0200

    Time: 298 ms (33.32%), Space: 50.5 MB (57.04%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+        var Dic = new Dictionary<string, List<string>>();
+
+            foreach (var s in strs)
+            {
+                char[] charArray = s.ToCharArray();
+                Array.Sort(charArray);
+                string s2 = new string(charArray);
+                
+
+                if (Dic.ContainsKey(s2))
+                {
+                    Dic[s2].Add(s);
+                  
+                }
+                else
+                {
+                    Dic[s2] = new List<string>()
+                    {
+                        s
+                    };
+                }
+            }
+
+
+            var t= Dic.Select(x => x.Value) ;
+            List<IList<string>> n = new List<IList<string>>();
+            foreach (var item in t)
+            {
+                n.Add(item);
+            }
+            return n;
+
+    }
+}
