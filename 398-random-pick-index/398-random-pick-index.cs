199beaa32acaeb46fdbe2d1fde2d30fbfd352281
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-06-22 09:39:02 +0200

    Time: 933 ms (16.67%), Space: 62.4 MB (6.67%) - LeetHub

---
+++ 398-random-pick-index/398-random-pick-index.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    int[] nums ;
+  Dictionary<int, List<int>> dic = new Dictionary<int, List<int>>();
+
+    public Solution(int[] nums) {
+        this.nums= nums;
+        
+          for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    dic[nums[i]].Add(i);
+                }
+                else
+                {
+                    dic[nums[i]] = new List<int>() { i };
+                }
+
+            }
+    }
+    
+    public int Pick(int target) {
+      var d=dic.Where(x => x.Key == target).Select(x => x.Value).FirstOrDefault().ToList();
+
+            Random random = new Random();
+           int k= random.Next(0, d.Count);
+
+            return d[k];       
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.Pick(target);
+ */
