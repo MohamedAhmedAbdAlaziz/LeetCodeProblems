118c2a545c3cddfb2270b73e6c4bcf718f90f005
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-07-11 16:44:44 +0200

    Time: 113 ms (59.40%), Space: 38.1 MB (21.69%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cs
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cs
@@ -1,8 +1,43 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-           for (int i = 0; i < nums.Length; i++)
+             int left = 0;
+            int right = nums.Length-1;
+            while (left<= right)
             {
-                if (nums[i]== target) return i;
+                
+                int middle = left + ((right- left)/ 2);
+                if(target == nums[middle]) return middle;
+                else if(nums[middle] > nums[left])
+                {
+                    if ( nums[left] <= target && target < nums[middle])
+                    {
+                        right = middle - 1;
+                    }
+                    else
+                    {
+                        left = middle + 1;
+                    }
+                }
+
+                 else if(nums[middle] < nums[right])
+                {
+                    if (nums[middle] < target && target <= nums[right])
+                    {
+                        left = middle + 1;
+                    }
+                    else
+                    {
+                        right = middle - 1;
+                    }
+                }
+                else
+                {
+                    if (nums[left] == nums[middle]) left++;
+                    if (nums[right] == nums[middle]) right--;
+                }
+               
+                    
+
             }
             return -1;
     }
