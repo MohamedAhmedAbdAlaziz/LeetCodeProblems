eb1c2b35c65427d19f55e98e639ce60effb4bdae
Mohamed Sallam <74076841+MohamedAhmedAbdAlaziz@users.noreply.github.com>
2022-07-12 14:11:16 +0200

    Time: 226 ms (14.60%), Space: 45.3 MB (33.12%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MinSubArrayLen(int target, int[] nums) {
+             
+          int sum = 0;
+            int left = 0;
+            int right = 0;
+            int v = 0;
+            int res = nums.Length +1;
+           while(v<= nums.Length-1 && right <= nums.Length - 1)
+            {
+               // if (target == nums[left]) return 1;
+                sum += nums[v];               
+                 while (sum>=target)
+                {
+                    res = Math.Min(res, (right - left) + 1);
+                    sum -= nums[left];
+                    left++;
+                }
+                v++;
+                right++;
+            }
+            if (res == nums.Length + 1)
+                return 0;
+            return res;
+    }
+}
